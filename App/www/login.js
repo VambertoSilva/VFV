cc9349a56a68ebecb9ac09a509daec7da935bfa5
Vamberto Silva <athelstanmorchiori@gmail.com>
2022-07-05 09:09:26 -0300

    Login, e cadastros implementados

--- App/www/login.js
+++ App/www/login.js
@@ -1,8 +1,17 @@
-const btnLogin = document.getElementsById("btnLogin");
-const pageLogin = document.getElementsById("Login");
-const pagePrincipal = document.getElementsById("Principal");
+let = currView;
 
-btnLogin.addEventListener("click", () => {
-  pageLogin.classList.add("hidden");
-  pagePrincipal.classList.remove("hidden");
-});
+function trocaTela(tela) {
+  document.getElementById(currView).style.display = "none";
+  currView = tela;
+  document.getElementById(currView).style.display = "block";
+}
+
+currView = "login";
+
+function ShowCadastro() {
+  trocaTela("pagecadastro");
+}
+
+function ShowPrincipal() {
+  trocaTela("Principal");
+}
